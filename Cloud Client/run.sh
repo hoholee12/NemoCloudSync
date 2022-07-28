ed74b5740341a3e1dcfb94b7e7e7cd1cd53970b8
( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃ <hoholee12@naver.com>
2022-07-28 15:03:12 +0900

    Fix file not syncing on cloud client

--- Cloud Client/run.sh
+++ Cloud Client/run.sh
@@ -4,5 +4,6 @@ if [[ $(which docker) == "" ]]; then
 	sudo apt install docker.io -y
 	sudo docker pull owncloud/client
 fi
-sudo docker run -it -v /syncfolder:/syncfolder owncloud/client /bin/bash -c 'while true; do sleep 1; owncloudcmd -u admin -p admin /syncfolder http://115.145.170.217:8080 2>/dev/null; done' &
-sudo nautilus /syncfolder
+mkdir $HOME/syncfolder
+sudo docker run -v $HOME/syncfolder:/syncfolder owncloud/client /bin/bash -c 'while true; do sleep 1; owncloudcmd -u admin -p admin /syncfolder http://115.145.170.217:8080 2>/dev/null; done' &
+sudo nautilus $HOME/syncfolder
